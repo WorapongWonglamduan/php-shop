395e689240461a3581b0c51f8ef7ff8ad1eec534
WorapongWonglamduan <the_oo_ooo@hotmail.com>
2024-07-07 18:30:00 +0700

    update

--- index.php
+++ index.php
@@ -3,55 +3,72 @@ include("condb.php");
 include("config.php");
 session_start();
 
-// if (!isset($_SESSION['username'])) {
-//     header('location: login.php');
-// }
 $sql = "SELECT * FROM products";
 $query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
 ?>
 <!doctype html>
 <html lang="en">
 
 <?php
-include("./includes/head.php")
-?>
-
-<?php
-include("./includes/navbar.php")
+include("./includes/head.php");
+include("./includes/navbar.php");
 ?>
 
 <body>
   <div class="container">
-    <!-- <div class="alert alert-primary h4 mt-4" role="alert">
-            Welcome <?php echo $_SESSION["firstname"] ?>
-        </div> -->
-    <!-- <?php
-          // if (isset($_SESSION["firstname"])) {
-          //     echo "<div class='text-success'>";
-          //     echo $_SESSION["firstname"] . " " . $_SESSION["lastname"];
-          //     echo '</div>';
-          // }
-          ?> -->
-    <!-- <br />
-        <a href="logout.php" class="alert alert-primary">Logout</a> -->
-
-
     <div class="row mt-4">
       <?php
-      while ($product = $query->fetch_object()) :
-      ?>
-        <div class="col-md-3 mb-4 p-4">
-          <img src="<?php echo $uploadsPath . $product->images ?>" width="200" height="250" style="object-fit: cover">
-          <br />
-          <div>ProductId :<?php echo $product->product_id ?></div>
-          <h6 style="white-space: nowrap;text-overflow: ellipsis;overflow: hidden;font-weight:bold"><?php echo $product->product_name ?></h6>
-          <div>Price :<?php echo number_format($product->price, 2) ?> ฿</div>
-        </div>
-      <?php endwhile; ?>
-      <?php mysqli_close($conn) ?>
+      $perpage = 6;
+      if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+      } else {
+        $page = 1;
+      }
+      $start = ($page - 1) * $perpage;
+      $sql = "SELECT * FROM products ORDER BY product_id LIMIT {$start}, {$perpage}";
+      $queryProduct = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 
+      while ($product = mysqli_fetch_assoc($queryProduct)) {
+        $price = $product["price"];
+      ?>
+        <div class="col-md-4 mb-4 p-4">
+          <img src="<?php echo $uploadsPath . $product['images']; ?>" width="200" height="250" style="object-fit: cover;">
+          <br />
+          <div>Product ID: <?php echo $product['product_id']; ?></div>
+          <h6 style="white-space: nowrap; text-overflow: ellipsis; overflow: hidden; font-weight: bold;"><?php echo $product['product_name']; ?></h6>
+          <div>Price: <?php echo number_format($product['price'], 2); ?> ฿</div>
+          <a href="#" class="btn btn-info">Add</a>
+        </div>
+      <?php } ?>
     </div>
+    <!-- end row -->
+
+    <?php
+    $sqlProductCount = "SELECT COUNT(*) AS total FROM products";
+    $resultProductCount = mysqli_query($conn, $sqlProductCount);
+    $rowProductCount = mysqli_fetch_assoc($resultProductCount);
+    $totalProducts = $rowProductCount['total'];
+    $total_page = ceil($totalProducts / $perpage);
+    ?>
+
+    <nav aria-label="Page navigation example">
+      <ul class="pagination justify-content-center">
+        <li class="page-item <?php if ($page <= 1) echo 'disabled'; ?>">
+          <a class="page-link" href="index.php?page=<?php echo ($page > 1) ? ($page - 1) : 1; ?>">Previous</a>
+        </li>
+        <?php for ($i = 1; $i <= $total_page; $i++) { ?>
+          <li class="page-item <?php if ($i == $page) echo 'active'; ?>"><a class="page-link" href="index.php?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+        <?php } ?>
+        <li class="page-item <?php if ($page >= $total_page) echo 'disabled'; ?>">
+          <a class="page-link" href="index.php?page=<?php echo ($page < $total_page) ? ($page + 1) : $total_page; ?>">Next</a>
+        </li>
+      </ul>
+    </nav>
   </div>
+  <!-- container -->
 </body>
 
 </html>
+
+<?php mysqli_close($conn); ?>
